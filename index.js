d5a30293c486fb49a58601210f06e9a479e8c0c1
jiavan <jiavan.com@gmail.com>
2019-07-12 23:47:05 +0800

    release v1.1.1

---
+++ index.js
@@ -0,0 +1,1 @@
+!function(e,t){var n=t.documentElement,d=e.devicePixelRatio||1;function i(){var e=n.clientWidth/10;n.style.fontSize=e+"px",n.setAttribute("data-dpr",d)}!function e(){t.body?t.body.style.fontSize=12*d+"px":t.addEventListener("DOMContentLoaded",e)}(),i(),e.addEventListener("resize",i),e.addEventListener("pageshow",function(e){e.persisted&&i()})}(window,document);
